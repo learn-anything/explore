bda31f2a1ac725a03d5af23bee93a069866246bc
Nikita <nikita.voloboev@gmail.com>
2024-03-29 17:21:46 +0400

    .

--- cli/cli.ts
+++ cli/cli.ts
@@ -9,89 +9,89 @@
 // TODO: use typed mobius client
 
 // make @la/shared useable from explore repo
-import { createMobiusFromApiToken } from "@la/shared/lib";
-import open from "open";
-import { createInterface } from "readline/promises";
-import fs from "fs";
-import path from "path";
+import { createMobiusFromApiToken } from "@la/shared/lib"
+import open from "open"
+import { createInterface } from "readline/promises"
+import fs from "fs"
+import path from "path"
 
 async function main() {
-  const args = Bun.argv;
+  const args = Bun.argv
   const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
-  });
+  })
 
-  const command = args[2];
-  const commandParts = command?.split("=") || [];
-  const commandKey = commandParts[0];
+  const command = args[2]
+  const commandParts = command?.split("=") || []
+  const commandKey = commandParts[0]
 
   if (process.env.API_TOKEN === undefined) {
     await rl.question(
       "API_TOKEN not set, press return to open browser to set it"
-    );
-    await open("http://localhost:3000/get-api-key");
-    let server;
+    )
+    await open("http://localhost:3000/get-api-key")
+    let server
     const token = await new Promise<string | null>((resolve) => {
       server = Bun.serve({
         port: 3500,
         fetch(req) {
-          resolve(new URL(req.url).searchParams.get("token"));
+          resolve(new URL(req.url).searchParams.get("token"))
           return new Response(
             `<link rel="icon" href="data:;base64,iVBORw0KGgo="><script>window.close()</script>`,
             { headers: { "Content-Type": "text/html" } }
-          );
+          )
         },
-      });
-    });
-    console.log(token, "token");
-    rl.close();
-    server!.stop();
-    return "";
+      })
+    })
+    console.log(token, "token")
+    rl.close()
+    server!.stop()
+    return ""
   }
   if (process.env.GRAFBASE_API_URL === undefined) {
     console.log(
       "GRAFBASE_API_URL not set, press return to open browser to set it"
-    );
-    return "";
+    )
+    return ""
   }
   const mobius = createMobiusFromApiToken(
     process.env.API_TOKEN,
     process.env.GRAFBASE_API_URL
-  );
-  console.log(mobius);
+  )
+  console.log(mobius)
 
   switch (commandKey) {
     case "--safari-url": {
-      const url = commandParts[1];
-      break;
+      const url = commandParts[1]
+      break
     }
     case "--chrome-url": {
-      const url = commandParts[1];
-      break;
+      const url = commandParts[1]
+      break
     }
     case "--text": {
-      const text = commandParts[1];
-      break;
+      const text = commandParts[1]
+      break
     }
     case "--text-with-safari-url": {
-      const text = commandParts[1];
-      break;
+      const text = commandParts[1]
+      break
     }
     case "--text-with-chrome-url": {
-      const text = commandParts[1];
-      break;
+      const text = commandParts[1]
+      break
     }
     case undefined:
-      console.log("No command provided");
-      break;
+      console.log("No command provided")
+      break
     default:
-      console.log("Unknown command");
-      break;
+      console.log("Unknown command")
+      break
   }
 }
 
-await main();
+await main()
 
 function updateEnvStore(token: string) {
   const filePath = path.join(
@@ -99,14 +99,14 @@ function updateEnvStore(token: string) {
     ".config",
     "la",
     ".env"
-  );
+  )
 
   if (!fs.existsSync(filePath)) {
-    fs.mkdirSync(path.dirname(filePath), { recursive: true });
-    fs.writeFileSync(filePath, "");
-    Bun.write(filePath, `token=${token}`);
+    fs.mkdirSync(path.dirname(filePath), { recursive: true })
+    fs.writeFileSync(filePath, "")
+    Bun.write(filePath, `token=${token}`)
   } else {
-    Bun.write(filePath, `token=${token}`);
+    Bun.write(filePath, `token=${token}`)
   }
 }
 
@@ -116,15 +116,15 @@ function getTokenFromEnvStore() {
     ".config",
     "la",
     ".env"
-  );
+  )
 
   if (fs.existsSync(filePath)) {
-    const envFileContent = fs.readFileSync(filePath, "utf8");
-    const token = envFileContent.split("=")[1];
-    return token;
+    const envFileContent = fs.readFileSync(filePath, "utf8")
+    const token = envFileContent.split("=")[1]
+    return token
   } else {
-    console.log("File does not exist");
-    return null;
+    console.log("File does not exist")
+    return null
   }
 }
 
